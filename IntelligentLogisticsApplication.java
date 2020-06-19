381d727770f3ff4f9a908234d271134f273d91d2
ccc-hhh <55867067+ccc-hhh@users.noreply.github.com>
2020-06-19 18:55:47 +0800

    Add files via upload

---
+++ IntelligentLogisticsApplication.java
@@ -0,0 +1,13 @@
+package cn.uestc.software.intelligentlogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentLogisticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentLogisticsApplication.class, args);
+    }
+
+}
